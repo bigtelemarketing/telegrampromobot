a18a298ee9211147ca410ac0e367799b9e7ae1c3
bigtelemarketing <80102760+bigtelemarketing@users.noreply.github.com>
2025-05-21 21:21:46 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -67,5 +67,5 @@ if __name__ == "__main__":
     app.run_webhook(
         listen="0.0.0.0",
         port=port,
-        webhook_url=f"https://telegrampromobot-02eo.onrender.com"
+        webhook_url=f"https://telegrampromobot-02eo.onrender.com/"
     )
